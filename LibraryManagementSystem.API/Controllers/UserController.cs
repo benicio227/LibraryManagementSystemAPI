c1409f929a638b3a5d1af44f8ccfbff327534e9b
benicio <beniciobrandao@hotmail.com>
2025-04-12 22:48:24 -0300

    Controller de Usuário criado

---
+++ LibraryManagementSystem.API/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Post()
+    {
+        return Ok();
+    }
+
+}
