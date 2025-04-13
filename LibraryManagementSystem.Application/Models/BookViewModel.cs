defd11ea1486539c8ad5fcf5377e9851b7b453b9
benicio <beniciobrandao@hotmail.com>
2025-04-13 14:27:53 -0300

    Cria classe BookViewModel para retornar dados ao front

---
+++ LibraryManagementSystem.Application/Models/BookViewModel.cs
@@ -0,0 +1,25 @@
+﻿using LibraryManagementSystem.Core.Entities;
+
+namespace LibraryManagementSystem.Application.Models;
+public class BookViewModel
+{
+    public BookViewModel(int id, string title, string author, string isbn, int published)
+    {
+        Id = id;
+        Title = title;
+        Author = author;
+        Isbn = isbn;
+        Published = published;
+    }
+
+    public int Id { get; private set; }
+    public string Title { get; private set; }
+    public string Author { get; private set; }
+    public string Isbn { get; private set; }
+    public int Published { get; private set; }
+
+    public static BookViewModel FromEntity(Book book)
+    {
+        return new BookViewModel(book.Id, book.Title, book.Author, book.Isbn, book.Published);
+    }
+}
