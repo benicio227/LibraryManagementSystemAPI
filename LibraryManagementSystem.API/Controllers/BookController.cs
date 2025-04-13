e59af70a760f03a550108765ee7b25bc5d07ad56
benicio <beniciobrandao@hotmail.com>
2025-04-12 22:49:08 -0300

    Refatora

--- LibraryManagementSystem.API/Controllers/BookController.cs
+++ LibraryManagementSystem.API/Controllers/BookController.cs
@@ -1,9 +1,31 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryManagementSystem.API.Controllers;
 [Route("api/[controller]")]
 [ApiController]
 public class BookController : ControllerBase
 {
+    [HttpPost]
+    public IActionResult Post()
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    public IActionResult GetById()
+    {
+        return Ok();
+    }
+
+    [HttpDelete]
+    public IActionResult Delete()
+    {
+        return Ok();
+    }
 }
