68e5bf37b37195bbbcfaf4b191178dbabfe22033
benicio <beniciobrandao@hotmail.com>
2025-04-13 13:34:34 -0300

    Adiciona serviço do MediatR

--- LibraryManagementSystem.API/Controllers/UserController.cs
+++ LibraryManagementSystem.API/Controllers/UserController.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LibraryManagementSystem.Application.Commands.UserFolder.InsertUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryManagementSystem.API.Controllers;
 [Route("api/[controller]")]
 [ApiController]
 public class UserController : ControllerBase
 {
-    [HttpPost]
-    public IActionResult Post()
+    private readonly IMediator _mediator;
+    public UserController(IMediator mediator)
     {
-        return Ok();
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(InsertUserCommand command)
+    {
+        var result = await _mediator.Send(command);
+
+        return Created(string.Empty, result);
     }
 
 }
