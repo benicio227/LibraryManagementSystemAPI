8c5a70e8cc7466f1a7a918b1b2e69d1176be0b10
benicio <beniciobrandao@hotmail.com>
2025-04-13 14:48:42 -0300

    Cria e implementa o acesso a dados para entidade Book

---
+++ LibraryManagementSystem.Infrastructure/Repositories/BookRepository.cs
@@ -0,0 +1,43 @@
+﻿using LibraryManagementSystem.Core.Entities;
+using LibraryManagementSystem.Core.Repositories;
+using LibraryManagementSystem.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Infrastructure.Repositories;
+public class BookRepository : IBookRepository
+{
+    private readonly LibraryManagementSystemDbContext _context;
+
+    public BookRepository(LibraryManagementSystemDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<Book?> Add(Book book)
+    {
+        await _context.Books.AddAsync(book);
+        await _context.SaveChangesAsync();
+
+        return book;
+    }
+
+    public async Task Delete(int id)
+    {
+        var book = await _context.Books.SingleOrDefaultAsync(b => b.Id == id);
+
+        _context.Books.Remove(book!);
+    }
+
+    public async Task<List<Book>> GetAll()
+    {
+        var books = await _context.Books.ToListAsync();
+
+        return books;
+    }
+
+    public async Task<Book?> GetById(int id)
+    {
+        var book = await _context.Books.SingleOrDefaultAsync(b => b.Id == id);
+
+        return book;
+    }
+}
