aeccc8d3fe2347f27b80dc7588851af0fb17d0b5
benicio <beniciobrandao@hotmail.com>
2025-04-13 18:03:38 -0300

    Cria a pasta GetBookById para criar o Handler e usando o MediatR

---
+++ LibraryManagementSystem.Application/Queries/GetBookById/GetBookByIdHandler.cs
@@ -0,0 +1,27 @@
+﻿using LibraryManagementSystem.Application.Models;
+using LibraryManagementSystem.Core.Repositories;
+using MediatR;
+
+namespace LibraryManagementSystem.Application.Queries.GetBookById;
+public class GetBookByIdHandler : IRequestHandler<GetBookByIdQuery, BookViewModel>
+{
+    private readonly IBookRepository _repository;
+
+    public GetBookByIdHandler(IBookRepository repository)
+    {
+        _repository = repository;
+    }
+    public async Task<BookViewModel> Handle(GetBookByIdQuery request, CancellationToken cancellationToken)
+    {
+        var book = await _repository.GetById(request.Id);
+
+        if (book is null)
+        {
+            throw new KeyNotFoundException("Livro não encontrado.");
+        }
+
+        var model = BookViewModel.FromEntity(book);
+
+        return model;
+    }
+}
---
+++ LibraryManagementSystem.Application/Queries/GetBookById/GetBookByIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using LibraryManagementSystem.Application.Models;
+using MediatR;
+
+namespace LibraryManagementSystem.Application.Queries.GetBookById;
+public class GetBookByIdQuery : IRequest<BookViewModel>
+{
+    public GetBookByIdQuery(int id)
+    {
+        Id = id;
+    }
+
+    public int Id { get; set; }
+}
