7480fd588af5cc70b107c95fb382ee870d18b885
benicio <beniciobrandao@hotmail.com>
2025-04-13 13:41:03 -0300

    Cria a pasta Repositories e o acesso a dados

---
+++ LibraryManagementSystem.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using LibraryManagementSystem.Core.Entities;
+using LibraryManagementSystem.Core.Repositories;
+using LibraryManagementSystem.Infrastructure.Persistence;
+
+namespace LibraryManagementSystem.Infrastructure.Repositories;
+public class UserRepository : IUserRepository
+{
+    public readonly LibraryManagementSystemDbContext _context;
+
+    public UserRepository(LibraryManagementSystemDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<User?> Add(User user)
+    {
+        await _context.AddAsync(user);
+        await _context.SaveChangesAsync();
+
+        return user;
+    }
+}
