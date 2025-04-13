46854a048df1a07fb850d1500cda26df027cd335
benicio <beniciobrandao@hotmail.com>
2025-04-13 08:16:01 -0300

    Cria projeto Infrastructure e configura o DbContext

---
+++ LibraryManagementSystem.Infrastructure/Persistence/LibraryManagementSystemDbContext.cs
@@ -0,0 +1,33 @@
+﻿using LibraryManagementSystem.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Infrastructure.Persistence;
+public class LibraryManagementSystemDbContext : DbContext
+{
+    public LibraryManagementSystemDbContext(DbContextOptions<LibraryManagementSystemDbContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Loan> Loans { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Loan>()
+            .HasOne(l => l.User)
+            .WithMany(u => u.Loans)
+            .HasForeignKey(l => l.IdUser)
+            .OnDelete(DeleteBehavior.Restrict);
+
+
+
+        builder.Entity<Loan>()
+            .HasOne(l => l.Book)
+            .WithMany(b => b.Loans)
+            .HasForeignKey(l => l.Book)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        base.OnModelCreating(builder);
+    }
+}
