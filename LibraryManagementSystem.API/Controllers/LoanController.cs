fef4426af34e3629e6f35695b4ea05fb946d2575
benicio <beniciobrandao@hotmail.com>
2025-04-12 22:47:40 -0300

    Controller de Emprestimo criado

---
+++ LibraryManagementSystem.API/Controllers/LoanController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class LoanController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Post()
+    {
+        return Ok();
+    }
+
+    [HttpPatch]
+    public IActionResult Patch()
+    {
+        return Ok();
+    }
+}
