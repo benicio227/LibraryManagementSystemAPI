d3ff2bd22f6f1e40ab4219860c553e3de87ac2eb
benicio <beniciobrandao@hotmail.com>
2025-04-14 17:26:20 -0300

    Cria o arquivo ResultViewModel para personalizar as mensagens de retorno

---
+++ LibraryManagementSystem.Application/Models/ResultViewModel.cs
@@ -0,0 +1,42 @@
+﻿namespace LibraryManagementSystem.Application.Models;
+public class ResultViewModel
+{
+    public ResultViewModel(bool isSuccess = true, string message = "")
+    {
+        IsSuccess = isSuccess;
+        Message = message;
+    }
+
+    public bool IsSuccess { get; set; }
+    public string Message { get; set; }
+
+    public static ResultViewModel Success()
+    {
+        return new();
+    }
+
+    public static ResultViewModel Error(string message)
+    {
+        return new(false, message);
+    }
+}
+
+public class ResultViewModel<T> : ResultViewModel
+{
+    public ResultViewModel(T? data, bool isSuccess = true, string message = "") : base(isSuccess, message)
+    {
+        Data = data;
+    }
+
+    public T? Data { get; set; }
+
+    public static ResultViewModel<T> Success(T data)
+    {
+        return new ResultViewModel<T>(data);
+    }
+
+    public static ResultViewModel<T> Error(string message)
+    {
+        return new(default, false, message);
+    }
+}
