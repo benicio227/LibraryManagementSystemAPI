d66990ab20b47f30e2df5e230f06c8168843b5b0
benicio <beniciobrandao@hotmail.com>
2025-04-14 08:52:10 -0300

    Cria o Handler para remover um livro

---
+++ LibraryManagementSystem.Application/Commands/BookFolder/DeleteBook/DeleteBookCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace LibraryManagementSystem.Application.Commands.BookFolder.DeleteBook;
+public class DeleteBookCommand : IRequest<Unit>
+{
+    public DeleteBookCommand(int id)
+    {
+        Id = id;
+    }
+
+    public int Id { get; set; }
+}
---
+++ LibraryManagementSystem.Application/Commands/BookFolder/DeleteBook/DeleteBookHandle.cs
@@ -0,0 +1,22 @@
+﻿using LibraryManagementSystem.Core.Repositories;
+using MediatR;
+
+namespace LibraryManagementSystem.Application.Commands.BookFolder.DeleteBook;
+public class DeleteBookHandle : IRequestHandler<DeleteBookCommand, Unit>
+{
+    private readonly IBookRepository _repository;
+
+    public DeleteBookHandle(IBookRepository repository)
+    {
+        _repository = repository;
+    }
+    public async Task<Unit> Handle(DeleteBookCommand request, CancellationToken cancellationToken)
+    {
+        var deleted = await _repository.Delete(request.Id);
+
+        if (!deleted)
+            throw new KeyNotFoundException("Livro não encontrado para exclusão.");
+
+        return Unit.Value;
+    }
+}
