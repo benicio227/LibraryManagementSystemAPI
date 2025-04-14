a4302182ff3663a34a461876fc15ac7860c6113d
benicio <beniciobrandao@hotmail.com>
2025-04-14 08:59:09 -0300

    Adiciona método MarkAsReturn para informar se empréstimo está dentro do prazo

--- LibraryManagementSystem.Core/Entities/Loan.cs
+++ LibraryManagementSystem.Core/Entities/Loan.cs
@@ -14,8 +14,17 @@ public class Loan
     public int IdBook {  get; private set; }
     public DateTime DateLoan { get; private set; }
     public DateTime DateDue { get; private set; }
-    public DateTime? DateReturned { get; private set; }
+    public DateTime? DateReturned { get; protected set; }
 
     public User User { get; private set; }
     public Book Book { get; private set; }
+
+    public int MarkAsReturned()
+    {
+        DateReturned = DateTime.Now;
+
+        var atraso = (DateReturned.Value - DateDue).Days;
+
+        return atraso > 0 ? atraso : 0;
+    }
 }
