86d03506ab8018f11a20d907ee8c959876ed6d84
benicio <beniciobrandao@hotmail.com>
2025-04-14 08:54:16 -0300

    Cria Handler para emprestar livro

---
+++ LibraryManagementSystem.Application/Models/LoanViewModel.cs
@@ -0,0 +1,22 @@
+﻿using LibraryManagementSystem.Core.Entities;
+
+namespace LibraryManagementSystem.Application.Models;
+public class LoanViewModel
+{
+    public LoanViewModel(int id, int idUser, int idBook, DateTime dateDue)
+    {
+        Id = id;
+        IdUser = idUser;
+        IdBook = idBook;
+        DateDue = dateDue;
+    }
+    public int Id { get; private set; }
+    public int IdUser { get; private set; }
+    public int IdBook { get; private set; }
+    public DateTime DateDue { get; private set; }
+
+    public static LoanViewModel FromEntity(Loan loan)
+    {
+        return new LoanViewModel(loan.Id, loan.IdUser, loan.IdBook, loan.DateDue);
+    }
+}
