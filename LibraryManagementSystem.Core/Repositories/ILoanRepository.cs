7525a05f96ba5880a4b96153c019a1f4902e024f
benicio <beniciobrandao@hotmail.com>
2025-04-14 08:55:59 -0300

    Cria a interface para ILoanRepository

---
+++ LibraryManagementSystem.Core/Repositories/ILoanRepository.cs
@@ -0,0 +1,8 @@
+﻿using LibraryManagementSystem.Core.Entities;
+
+namespace LibraryManagementSystem.Core.Repositories;
+public interface ILoanRepository
+{
+    public Task<Loan?> Add(Loan loan);
+    public Task<Loan?> Pacth(int id);
+}
