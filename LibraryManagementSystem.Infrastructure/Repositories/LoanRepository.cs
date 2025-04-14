51b8a72a1a900a0d0226928ed2e68d49b5619eef
benicio <beniciobrandao@hotmail.com>
2025-04-14 08:56:49 -0300

    Cria e define os métodos para acesso a dados

---
+++ LibraryManagementSystem.Infrastructure/Repositories/LoanRepository.cs
@@ -0,0 +1,38 @@
+﻿using LibraryManagementSystem.Core.Entities;
+using LibraryManagementSystem.Core.Repositories;
+using LibraryManagementSystem.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Infrastructure.Repositories;
+public class LoanRepository : ILoanRepository
+{
+    private readonly LibraryManagementSystemDbContext _context;
+
+    public LoanRepository(LibraryManagementSystemDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<Loan?> Add(Loan loan)
+    {
+        await _context.Loans.AddAsync(loan);
+        await _context.SaveChangesAsync();
+
+        return loan;
+    }
+
+    public async Task<Loan?> Pacth(int id)
+    {
+        var loan = await _context.Loans.FirstOrDefaultAsync(l => l.Id == id);
+
+        if (loan is null)
+        {
+            throw new KeyNotFoundException("Empréstimo não encontrado.");
+        }
+
+        loan.MarkAsReturned();
+
+        await _context.SaveChangesAsync();
+
+        return loan;
+    }
+}
