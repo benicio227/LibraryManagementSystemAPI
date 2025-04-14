b2f6b9090ffc26e88dbcf6bdaf721dbfd1c1eff7
benicio <beniciobrandao@hotmail.com>
2025-04-14 17:25:23 -0300

    Cria a pasta Validators para validar dados da requisição

---
+++ LibraryManagementSystem.Application/Validators/CreateUserValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using LibraryManagementSystem.Application.Commands.UserFolder.InsertUser;
+using Microsoft.Extensions.Hosting;
+
+namespace LibraryManagementSystem.Application.Validators;
+public class CreateUserValidator : AbstractValidator<InsertUserCommand>
+{
+    public CreateUserValidator()
+    {
+        RuleFor(u => u.Name)
+            .NotEmpty()
+                .WithMessage("Nome não pode ser vazio");
+
+        RuleFor(u => u.Email)
+            .EmailAddress()
+                .WithMessage("E-mail inválido");
+
+    }
+}
